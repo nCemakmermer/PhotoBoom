0bbbd2d5457974470be9ff7cebd25572fe229589
Cem Akmermer <cemakmermer2015@gmail.com>
2021-06-09 01:02:01 +0300

    Proje dosyası ekle.

---
+++ MyPhotos/Models/ImageDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPhotos.Models
+{
+    public class ImageDbContext : DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext> options) : base(options)
+        { }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}
---
+++ MyPhotos/Models/ImageModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPhotos.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Image")]
+        public string ImageName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Tags : ")]
+        public string Tags { get; set; }
+
+        [NotMapped]
+        [DisplayName("Upload File")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
